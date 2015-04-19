02d2120b45a9dfd785c653abd39ef5295ad2cf46
sez <sesha911@gmail.com>
2015-04-18 17:12:06 -0700

    Sesha: Ran lda on new corpus

--- lda_runs.py
+++ lda_runs.py
@@ -9,7 +9,7 @@ from gensim import corpora, models, similarities, matutils
 import gensim
 import numpy as np
 import scipy.stats as stats
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import math
 
 stops = [word for word in nltk.corpus.stopwords.words('english')]
@@ -18,17 +18,17 @@ my_corpus = gensim.corpora.MmCorpus('abstracts.mm')
 dictionary = gensim.corpora.Dictionary.load('abstracts.dict')
 kl=[]
 
-"""lda = gensim.models.ldamodel.LdaModel(corpus=my_corpus,
-        id2word=dictionary,num_topics=122,passes=10)
-"""
-lda=gensim.models.ldamodel.LdaModel.load("abstracts")
+lda = gensim.models.ldamodel.LdaModel(corpus=my_corpus,
+        id2word=dictionary,num_topics=122,passes=15)
+
+# lda=gensim.models.ldamodel.LdaModel.load("abstracts")
 kl=lda.print_topics(122)
     
 
 
 #lda.save("abstracts")
 
-with open('topics.txt','w') as f:
+with open('topics.txt','w+') as f:
     for line in kl:
         f.write(line)
         f.write('\n')
